89dccae10910286c18bea9f81f45e61d02b4ee3b
anedonia <deuxvys@gmail.com>
2021-05-03 14:42:26 +0200

    switch non terminé pour qu'il soit adapté en fonction de clement

--- controler/index.php
+++ controler/index.php
@@ -1,47 +1,28 @@
 <?php 
-    if(isset($_GET['login_err']))
-    {
-        $err = htmlspecialchars($_GET['login_err']);
 
-        switch($err)
-        {
-            case 'password':
-            ?>
-                <div class="erreur">
-                    <strong>Erreur</strong> Mot de passe incorrect
-                </div>
-            <?php
-            break;
-
-            case 'user':
-            ?>
-                <div class="erreur">
-                    <strong>Erreur</strong> Username incorrect
-                </div>
-            <?php
-            break;
-
-            case 'already':
-            ?>
-                <div class="erreur">
-                    <strong>Erreur</strong> Compte non existant
-                </div>
-            <?php
-            break;
-        }
-    }
-    //include('../view/login.php');
-
-    	if (!isset($_POST["action"]))
+    	
+    if (!isset($_POST["action"]))
 	{
 		include("../view/login.php");
 	}
-
-	switch ($_POST["action"]) {
-		case  'questions.php':
-			include("questions.php");
-			break;
-				
-	}
-
+    else 
+    {    
+        switch ($_POST["action"]) {
+            case  'inscription.php':
+                include("../view/inscription.php");
+                break;
+            case  'questions.php':
+			    include("../view/inscription.php");
+			    break;
+		    case  'news.php':
+			    include("../view/inscription.php");
+			    break;
+		    case  'cd.php':
+			    include("../view/inscription.php");
+			    break;
+		    case  'about.php':
+			    include("../view/inscription.php");
+			    break;
+        }
+    }
 ?> 
