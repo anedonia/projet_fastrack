664e35726fcddf64bf3fc2a6ffdea27fa2a15a6b
Clément BOVIN <clement.bovin@gmail.com>
2021-05-03 13:12:20 +0200

    ajout de l'index

---
+++ controler/index.php
@@ -0,0 +1,34 @@
+<?php 
+    if(isset($_GET['login_err']))
+    {
+        $err = htmlspecialchars($_GET['login_err']);
+
+        switch($err)
+        {
+            case 'password':
+            ?>
+                <div class="erreur">
+                    <strong>Erreur</strong> Mot de passe incorrect
+                </div>
+            <?php
+            break;
+
+            case 'email':
+            ?>
+                <div class="erreur">
+                    <strong>Erreur</strong> Username incorrect
+                </div>
+            <?php
+            break;
+
+            case 'already':
+            ?>
+                <div class="erreur">
+                    <strong>Erreur</strong> Compte non existant
+                </div>
+            <?php
+            break;
+        }
+    }
+    include('../view/login.php');
+?> 
