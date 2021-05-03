cf3a8f85e7a354993d265374882241f8cb316d62
Clément BOVIN <clement.bovin@gmail.com>
2021-05-03 14:19:45 +0200

    view connexion et inscription

--- controler/index.php
+++ controler/index.php
@@ -1,40 +1,10 @@
-<?php 
-    if(isset($_GET['login_err']))
-    {
-        $err = htmlspecialchars($_GET['login_err']);
-
-        switch($err)
-        {
-            case 'password':
-            ?>
-                <div class="erreur">
-                    <strong>Erreur</strong> Mot de passe incorrect
-                </div>
-            <?php
-            break;
-
-            case 'user':
-            ?>
-                <div class="erreur">
-                    <strong>Erreur</strong> Username incorrect
-                </div>
-            <?php
-            break;
-
-            case 'already':
-            ?>
-                <div class="erreur">
-                    <strong>Erreur</strong> Compte non existant
-                </div>
-            <?php
-            break;
-        }
-    }
+<?php
     //include('../view/login.php');
 
     	if (!isset($_POST["action"]))
 	{
 		include("../view/login.php");
+        //ici login du controler le ../view/login est temporaire
 	}
 
 	switch ($_POST["action"]) {
