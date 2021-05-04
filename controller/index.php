e582e2934541b8af188b60817c6052cb6baf926d
anedonia <deuxvys@gmail.com>
2021-05-04 10:42:32 +0200

    try catch du routeur

--- controller/index.php
+++ controller/index.php
@@ -2,31 +2,37 @@
 
 <?php
     //include('../view/login.php');
-
-    	if (!isset($_POST["action"]))
-
+	try 
 	{
-		include("../view/login.php");
-        //ici login du controler le ../view/login est temporaire
+			if (!isset($_POST["action"]) || !isset($_SESSION))
+		{
+			include("../view/login.php");
+			//ici login du controler le ../view/login est temporaire
+		}
+		else 
+		{    
+			switch ($_POST["action"]) {
+				case  'inscription.php':
+					
+					include("../view/inscription.php");
+					break;
+				case  'questions.php':
+					include("../view/inscription.php");
+					break;
+				case  'news.php':
+					include("../view/inscription.php");
+					break;
+				case  'cd.php':
+					include("../view/inscription.php");
+					break;
+				case  'about.php':
+					include("../view/inscription.php");
+					break;
+			}
+		}
 	}
-    else 
-    {    
-        switch ($_POST["action"]) {
-            case  'inscription.php':
-                include("../view/inscription.php");
-                break;
-            case  'questions.php':
-			    include("../view/inscription.php");
-			    break;
-		    case  'news.php':
-			    include("../view/inscription.php");
-			    break;
-		    case  'cd.php':
-			    include("../view/inscription.php");
-			    break;
-		    case  'about.php':
-			    include("../view/inscription.php");
-			    break;
-        }
-    }
-?> 
+	catch(Exeception $e)
+	{
+		echo 'Erreur : '. $e->getMessage();
+	}
+  
