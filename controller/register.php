e3cb68fa334d9e2f61c64a7c02f5af0cefb07620
Robin <robinlethiec1@gmail.com>
2021-05-05 14:12:46 +0200

    ajout register

---
+++ controller/register.php
@@ -0,0 +1,30 @@
+<?php
+
+if(!empty($_POST['password']) AND !empty($_POST['username'])){
+    print_r($_POST);
+    require('model\register.php');
+    if(empty(bdd("SELECT `id_user` FROM `user`"))){
+        $id_client = 0;
+        echo 'bite';
+    }else{
+        $id = bdd("SELECT `id_user` FROM `user`");
+        $id_client = intval($id[count($id)-1]['id_user']+1);
+        echo 'bite2';
+    }
+    var_dump(bdd("SELECT `id_user` FROM `user`"));
+    bdd("INSERT INTO `user` (`id_user`, `prenom`, `nom`, `identifiant`, `mdp`, `mail`, `role`) VALUES (".$id_client.", NULL, NULL, '".$_POST['username']."', '".$_POST['password']."', NULL, NULL);");
+    $_POST['action'] = '';
+    header('Location: index.php');
+    exit();
+}
+else{
+    require('model\register.php');
+    print_r($_POST);
+    $page_css = "\"./public/style_login.css\"";
+    $title = "register";
+    ob_start();
+    $content = ob_get_clean();
+    require('view\register.php');
+}
+
+?>
--- view/register.php
+++ view/register.php
@@ -1,87 +1,21 @@
-<!DOCTYPE html>
-<html lang="fr">
-        <head>
-            <meta charset="UTF-8">
-            <link rel="stylesheet" type="text/css" href="./style.css">
-            <title>FASTRACK REGISTER</title>
-        </head>
-<body>
-        <div class="login-form">
-            <?php 
-                if(isset($_GET['reg_err']))
-                {
-                    $err = htmlspecialchars($_GET['reg_err']);
+<?php
+require('view\header.php');
+?>
 
-                    switch($err)
-                    {
-                        case 'success':
-                        ?>
-                            <div class="alert alert-success">
-                                <strong>Succès</strong> inscription réussie !
-                            </div>
-                        <?php
-                        break;
-
-                        case 'password':
-                        ?>
-                            <div class="alert alert-danger">
-                                <strong>Erreur</strong> mot de passe différent
-                            </div>
-                        <?php
-                        break;
-
-                        case 'email':
-                        ?>
-                            <div class="alert alert-danger">
-                                <strong>Erreur</strong> email non valide
-                            </div>
-                        <?php
-                        break;
-
-                        case 'email_length':
-                        ?>
-                            <div class="alert alert-danger">
-                                <strong>Erreur</strong> email trop long
-                            </div>
-                        <?php 
-                        break;
-
-                        case 'pseudo_length':
-                        ?>
-                            <div class="alert alert-danger">
-                                <strong>Erreur</strong> pseudo trop long
-                            </div>
-                        <?php 
-                        case 'already':
-                        ?>
-                            <div class="alert alert-danger">
-                                <strong>Erreur</strong> compte deja existant
-                            </div>
-                        <?php 
-
-                    }
-                }
-                ?>
+<div id="container">
+            <!-- zone de d'enregistrement -->
             
-            <form action="../controler/register.php" method="post">
-                <h2 class="text-center">Inscription</h2>       
-                <div class="form-group">
-                    <input type="text" name="pseudo" class="form-control" placeholder="Pseudo" required="required" autocomplete="off">
-                </div>
-                <div class="form-group">
-                    <input type="email" name="email" class="form-control" placeholder="Email" required="required" autocomplete="off">
-                </div>
-                <div class="form-group">
-                    <input type="password" name="password" class="form-control" placeholder="Mot de passe" required="required" autocomplete="off">
-                </div>
-                <div class="form-group">
-                    <input type="password" name="password_retype" class="form-control" placeholder="Confirmer le mot de passe" required="required" autocomplete="off">
-                </div>
-                <div class="form-group">
-                    <button type="submit" class="btn_inscription">Inscription</button>
-                    <p class="btn_retour"><a href="../controler/index.php">Retour</a></p>
-                </div>   
+            <form action="./index.php" method="POST">
+                <h1>Register</h1>
+                
+                <label><b>Nom d'utilisateur</b></label>
+                <input type="text" placeholder="Entrer le nom d'utilisateur" name="username" required>
+                <label><b>Mot de passe</b></label>
+                <input type="password" placeholder="Entrer le mot de passe" name="password" required>
+                <input type="submit" id='submit' value='inscription.php' name ='action'>
             </form>
         </div>
-        </body>
-</html>
+
+<?php
+require('view\footer.php');
+?>
