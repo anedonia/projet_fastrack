ac0f05b437d65ed4e0ba2bd472afdf90e5ef0731
anedonia <deuxvys@gmail.com>
2021-05-05 12:47:04 +0200

    requete de verif du user (modele)

--- model/login.php
+++ model/login.php
@@ -10,8 +10,31 @@ function bite()
 }
     bite();
 
+    function db_connect()
+    {
+        try
+        {
+            $pdo = new PDO('mysql:host=localhost;dbname=lib_4','root','');
+            $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
 
+            return $pdo;
+        }
+        catch (PDOException $e)
+        {
+            echo "bug lors de la co ac la bdd";
+        }
+    }
 
-?>
+    function user_check($mdp, $identifiant)
+    {
+        $bdd = db_connect();
+
+        $req = $bdd->query('SELECT id FROM user WHERE mdp = SHA1(:mdp) AND identifiant :identifiant');
+        $req = $bdd->prepare($sql);
+        $req->execute(['mdp' => $mdp ,'identifiant' => $identifiant]);
+
+        var_dump($req);
+    }
+
 
 
