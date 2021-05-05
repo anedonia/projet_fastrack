922757ebcc87c7316ec8141ae13632518815590d
Robin <robinlethiec1@gmail.com>
2021-05-05 22:03:10 +0200

    ajout du cookie "account_created" pour que la page de login marque que le compte a bien été enregistrer. il a une durée de vie de 20 sec.

--- controller/register.php
+++ controller/register.php
@@ -14,6 +14,7 @@ if(!empty($_POST['password']) AND !empty($_POST['username'])){
     var_dump(bdd("SELECT `id_user` FROM `user`"));
     bdd("INSERT INTO `user` (`id_user`, `prenom`, `nom`, `identifiant`, `mdp`, `mail`, `role`) VALUES (".$id_client.", '".$_POST['first_name']."', '".$_POST['full_name']."', '".$_POST['username']."', '".$_POST['password']."', '".$_POST['email']."', NULL);");
     $_POST['action'] = '';
+    setcookie("account_created", True, time()+20);
     header('Location: index.php');
     exit();
 }
