af25ef97883b5e9893fd472e68fe9e7b6544b126
Robin <robinlethiec1@gmail.com>
2021-05-05 21:40:49 +0200

    update du css login et register sont mtn dans la meme box c'est trop kawaiiiii

--- view/login.php
+++ view/login.php
@@ -1,30 +1,31 @@
 <?php require 'header.php'?>
-
-<div id="container">
-    <!-- zone de connexion -->
-    
-    <form action="./index.php" method="POST">
-        <h1>Connexion</h1>
+<div id="all">
+    <div id="container">
+        <!-- zone de connexion -->
         
-        <label><b>Nom d'utilisateur</b></label>
-        <input type="text" placeholder="Entrer le nom d'utilisateur" name="username" required>
-
-        <label><b>Mot de passe</b></label>
-        <input type="password" placeholder="Entrer le mot de passe" name="password" required>
-
-        <input type="submit" id='submit' value='LOGIN' >
-        <?php
-        if(isset($_GET['erreur'])){
-            $err = $_GET['erreur'];
-            if($err==1 || $err==2)
-                echo "<p style='color:red'>Utilisateur ou mot de passe incorrect</p>";
+        <form action="./index.php" method="POST">
+            <h1>Connexion</h1>
             
-        }
-        ?>
-    </form>
-    <form action="./index.php" method="POST">
-        <input type="submit" id='submit' value='inscription.php' name='action' >
-    </form>
+            <label><b>Nom d'utilisateur</b></label>
+            <input type="text" placeholder="Entrer le nom d'utilisateur" name="username" required>
+
+            <label><b>Mot de passe</b></label>
+            <input type="password" placeholder="Entrer le mot de passe" name="password" required>
+
+            <input type="submit" id='submit' value='LOGIN' >
+            <?php
+            if(isset($_GET['erreur'])){
+                $err = $_GET['erreur'];
+                if($err==1 || $err==2)
+                    echo "<p style='color:red'>Utilisateur ou mot de passe incorrect</p>";
+                
+            }
+            ?>
+        </form>
+        <form action="./index.php" method="POST">
+            <input type="submit" id='submit' value='inscription.php' name='action' >
+        </form>
+    </div>
 </div>
 
 <?php require 'footer.php'?> 
