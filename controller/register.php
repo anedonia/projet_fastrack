81fc4eaa381f7225c3ca4f2ee25fd59bfe63f45d
Robin <robinlethiec1@gmail.com>
2021-05-06 11:53:17 +0200

    ajout mdp sha1 + optimisation du code

--- controller/register.php
+++ controller/register.php
@@ -3,16 +3,8 @@
 if(!empty($_POST['password']) AND !empty($_POST['username'])){
     print_r($_POST);
     require('model\register.php');
-    if(empty(bdd("SELECT `id_user` FROM `user`"))){
-        $id_client = 0;
-        echo 'bite';
-    }else{
-        $id = bdd("SELECT `id_user` FROM `user`");
-        $id_client = intval($id[count($id)-1]['id_user']+1);
-        echo 'bite2';
-    }
     var_dump(bdd("SELECT `id_user` FROM `user`"));
-    bdd("INSERT INTO `user` (`id_user`, `prenom`, `nom`, `identifiant`, `mdp`, `mail`, `role`) VALUES (".$id_client.", '".$_POST['first_name']."', '".$_POST['full_name']."', '".$_POST['username']."', '".$_POST['password']."', '".$_POST['email']."', NULL);");
+    bdd("INSERT INTO `user` (`id_user`, `prenom`, `nom`, `identifiant`, `mdp`, `mail`, `role`) VALUES (NULL, '".$_POST['first_name']."', '".$_POST['full_name']."', '".$_POST['username']."', '".sha1($_POST['password'])."', '".$_POST['email']."', NULL);");
     $_POST['action'] = '';
     setcookie("account_created", True, time()+20);
     header('Location: index.php');
