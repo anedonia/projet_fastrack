a35051f9d0a2e1683437008c0463cf5073433f40
anedonia <deuxvys@gmail.com>
2021-05-06 10:36:44 +0200

    recup de login view (je l'ai del appremment)

---
+++ view/login.php
@@ -0,0 +1,32 @@
+  
+<?php require 'header.php'?>
+<div id="all">
+    <div id="container">
+        <!-- zone de connexion -->
+        
+        <form action="./index.php" method="POST">
+            <h1>Connexion</h1>
+            
+            <label><b>Nom d'utilisateur</b></label>
+            <input type="text" placeholder="Entrer le nom d'utilisateur" name="username" required>
+
+            <label><b>Mot de passe</b></label>
+            <input type="password" placeholder="Entrer le mot de passe" name="password" required>
+
+            <input type="submit" id='submit' value='LOGIN' >
+            <?php
+            if(isset($_GET['erreur'])){
+                $err = $_GET['erreur'];
+                if($err==1 || $err==2)
+                    echo "<p style='color:red'>Utilisateur ou mot de passe incorrect</p>";
+                
+            }
+            ?>
+        </form>
+        <form action="./index.php" method="POST">
+            <input type="submit" id='submit' value='INSCRIPTION' name='action' >
+        </form>
+    </div>
+</div>
+
+<?php require 'footer.php'?> 
