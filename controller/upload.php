8d50ce3102a94faf92d2ec48c3462121f4a7be33
Robin <robinlethiec1@gmail.com>
2021-05-17 20:46:17 +0200

    upload controller

---
+++ controller/upload.php
@@ -0,0 +1,39 @@
+<?php
+if (empty($_SESSION['id_user'])){
+    header('Location: index.php');
+    exit();
+}
+require('.\model\upload.php');
+$page_css = "\"./public/style_profil.css\"";
+$title = "account";
+$jour_mois = date("m.d.y");
+
+$heure = date("H:i");
+
+if (!empty($_POST['valider'])){
+    if(!empty($_POST['nom']) && !empty($_POST['auteur']) && !empty($_POST['description']) && !empty($_POST['prix']) && !empty(file_get_contents($_FILES['image']['tmp_name'])) && !empty(file_get_contents($_FILES['son']['tmp_name']))){
+        
+        var_dump($_POST);
+        var_dump(file_get_contents($_FILES['son']['tmp_name']));
+        var_dump(file_get_contents($_FILES['image']['tmp_name']));
+        ajout_musique(file_get_contents($_FILES['image']['tmp_name']), file_get_contents($_FILES['son']['tmp_name']), $_POST['nom'], $_POST['auteur'], $_POST['description'], $_POST['prix']);
+        ajout_auteur($_POST['auteur']);
+        ajout_fait_par($_POST['nom'], $_POST['auteur']);
+        //header('Location: index.php?action=account');
+        //exit();
+    }
+    else{
+        ob_start();
+        echo'veuillez remplir tous les champs svp';
+        $content = ob_get_clean();
+    }
+}
+else{
+    ob_start();
+    $content = ob_get_clean();
+    require('.\view\upload.php');
+}
+
+//limitation des options en fonction des roles
+
+?>
