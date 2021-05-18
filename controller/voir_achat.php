0f8455f4697709da2926b7b34f3e1f087317fec1
Robin <robinlethiec1@gmail.com>
2021-05-19 01:55:24 +0200

    modif redirection

--- controller/voir_achat.php
+++ controller/voir_achat.php
@@ -26,10 +26,10 @@ ob_start();
                 </form>
                 <a href='#' onclick='document.getElementById("voir_vente").submit()'>Ventes</a>
 
-                <form id="mettre_en_vente" action="./index.php" method='GET'>
-                    <input type="hidden" name="action" value="mettre_en_vente"/>
+                <form id="upload" action="./index.php" method='GET'>
+                    <input type="hidden" name="action" value="upload"/>
                 </form>
-                <a href='#' onclick='document.getElementById("Upload").submit()'>Upload</a>
+                <a href='#' onclick='document.getElementById("upload").submit()'>Upload</a>
 <?php
 $content = ob_get_clean();
 if ($_SESSION['role'] !== "vendeur")
