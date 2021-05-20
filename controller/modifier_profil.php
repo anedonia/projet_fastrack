27593ae9633aa48f4ac836e4a8f207fb6d337186
Robin <robinlethiec1@gmail.com>
2021-05-20 13:04:19 +0200

    modif

--- controller/modifier_profil.php
+++ controller/modifier_profil.php
@@ -18,8 +18,8 @@ if (empty($_COOKIE['auth'])){
         if($_POST['pseudo'] == $vpseudo && sha1($_POST['mdp']) == $vmdp){
             setcookie('auth', True, time()+180);
         }
-    header('Location: index.php?action=modifier_profil');
-    exit();
+        header('Location: index.php?action=modifier_profil');
+        exit();
     }else{
         ob_start();
         affichage_connexion();
@@ -27,26 +27,26 @@ if (empty($_COOKIE['auth'])){
     }
 }else{
 //apres connexion
-$prenom = select_user('prenom')['0']['0'];
-$nom = select_user('nom')['0']['0'];
-$identifiant = select_user('identifiant')['0']['0'];
-$mdp = '******';
-$mail = select_user('mail')['0']['0'];
-if (!empty($_POST)){
-    foreach ($_POST as $key => $value) {
-        if($value !== ''){
-            if ($key === 'mdp'){
-                update_user($key, sha1($value));
-                header('Location: index.php?action=modifier_profil');
-                exit();
-            }else{
-                update_user($key, $value);
-                header('Location: index.php?action=modifier_profil');
-                exit();
+    $prenom = select_user('prenom')['0']['0'];
+    $nom = select_user('nom')['0']['0'];
+    $identifiant = select_user('identifiant')['0']['0'];
+    $mdp = '******';
+    $mail = select_user('mail')['0']['0'];
+    if (!empty($_POST)){
+        foreach ($_POST as $key => $value) {
+            if($value !== ''){
+                if ($key === 'mdp'){
+                    update_user($key, sha1($value));
+                    header('Location: index.php?action=modifier_profil');
+                    exit();
+                }else{
+                    update_user($key, $value);
+                    header('Location: index.php?action=modifier_profil');
+                    exit();
+                }
             }
         }
     }
-}
 ob_start();
 affichage_modif($prenom, $nom, $identifiant, $mdp, $mail);
 $content = ob_get_clean();
